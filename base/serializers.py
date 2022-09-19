b09fc2a9d3e37e31d20498c765ac645034f9dbc0
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-09-19 14:52:13 -0500

    tags serializers

--- base/serializers.py
+++ base/serializers.py
@@ -56,6 +56,8 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class PostSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
+    Tags = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Post
         fields = "__all__"
