c12e977546c6b3dd796007abc9cf087186f3bb6b
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-09-20 22:10:54 -0500

    cloud db

--- ISAMC_Knowledge/settings.py
+++ ISAMC_Knowledge/settings.py
@@ -126,10 +126,10 @@ WSGI_APPLICATION = 'ISAMC_Knowledge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'isamcknowledge',
-        'USER': 'postgres',
-        'PASSWORD': "enero201998",
-        'HOST': "localhost",
+        'NAME': 'dbe3gqd053fn5e',
+        'USER': 'vmmuiivvqblkic',
+        'PASSWORD': "32eedcd1a2be734746b041861142e1e3387b8e4ee3b672d3c29c3d4ba4f69f61",
+        'HOST': "ec2-18-209-78-11.compute-1.amazonaws.com",
         'PORT': '5432'
     }
 }
